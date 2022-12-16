0624727848dac4799f599524bcd1b7b3a83d5aaa
dipti <dipti@DESKTOP-F1UMDA6.broadband>
2022-12-16 11:52:00 +0000

    Automation Test

---
+++ E2ESeleniumJenkins/src/testcase/tc3.java
@@ -0,0 +1,19 @@
+package testcase;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class tc3 {
+	
+	@Test
+	public void testcase1() {
+		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver1.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.xpath("//button[@title='Only allow essential cookies']")).click();
+		driver.findElement(By.id("email")).sendKeys("Hello");
+		driver.findElement(By.id("pass")).sendKeys("123456");
+	}
+
+}
